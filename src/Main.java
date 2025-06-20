8b2b485b69d899166a4ff92505f36eba9435af37
Arundhatip72 <arundhatip72>
2025-06-21 00:28:48 +0530

    Welcome to anotherone

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System!");
-
+        System.out.println ("Welcome to anotherone");
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
         }catch (ClassNotFoundException e){
